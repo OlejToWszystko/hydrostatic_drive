812926b56dd2c6df907eaf70fd431c6c3d721cca
Maciej Olejnik <maciej.olejnik88@gmail.com>
2017-01-25 11:47:19 +0100

    Uporządkowanie skryptu

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # hydrostatic_drive
+
+	# First commit
+
+	zainicjowanie projektu
+
+
+	# Uporządkowanie skryptu
+
+	dodanie opisów, usunięcie zbędnych komentarzy, zmieniono układ obiektu fig2 klasy Figure, uzupełniono plik README.md komentarzami
+	odnośnie commitów
--- symulacja_zaworu.py
+++ symulacja_zaworu.py
@@ -7,59 +7,45 @@
 
 from read_file import Measurment
 from matplotlib import pyplot as plt
-# from array import array
 
+# Utworzenie obiektu, który jest instancją klasy Measurment
+# Plik z pomiarami zaworu proporcjonalnego
 ZawProp = Measurment('85_2500.txt')
-# ZawProp.print_channels()
-# ZawProp.print_units()
-'''
-for value in ZawProp.values:
-	print(value)
-'''
 
-
-# print(ZawProp.values[5][:100])
-# print("Ilość elementów na liście : " + str(len(ZawProp.values)))
-# ZawProp.measurment_to_dict()
-# print(ZawProp.dictMeasurment)
-
-settings_pressure = {
-				'x' : ZawProp.values[0],
-				'y' : ZawProp.values[5],
-				's' : 1,
-				'c' : 'red',
-				'edgecolor' : 'None',
-				}
-				
-#plt.scatter(**settings_pressure)
-#plt.csd(array('f', ZawProp.values[0]), array('f', ZawProp.values[5]))
-'''
-pressure_chart = plt.subplot(121)
-pressure_chart.plot(ZawProp.values[0], ZawProp.values[5])
-others_chart = pressure_chart.twinx()
-others_chart.plot(ZawProp.values[0], ZawProp.values[4], c='r')
-others_chart.plot(ZawProp.values[0], ZawProp.values[3], c='green')
-others_chart.plot(ZawProp.values[0], ZawProp.values[2], c='pink')
-p_v = plt.subplot(122)
-p_v.plot(ZawProp.values[0], ZawProp.values[5])
-p_v.twinx().plot(ZawProp.values[0], ZawProp.values[1], c='green')
-'''
+# Utworzenie obiektów klasy Figure (1 obiekt) oraz Axes (dwa obiekty)
 fig1, (sp1, sp2) = plt.subplots(1, 2)
+
+# utworzenie serii danych p(t) na obiekcie sp1
 sp1.plot(ZawProp.values[0], ZawProp.values[5])
+
+# utworzenie nowego obiektu Axes, ze wspólną osią x z sp1
 sp1_other = sp1.twinx()
+
+# dodanie serii danych do obiektu sp1_other
 sp1_other.plot(ZawProp.values[0], ZawProp.values[4], c='r')
 sp1_other.plot(ZawProp.values[0], ZawProp.values[3], c='green')
 sp1_other.plot(ZawProp.values[0], ZawProp.values[2], c='pink')
+
+# utworzenie serii danych p(t) na obiekcie sp2
 sp2.plot(ZawProp.values[0], ZawProp.values[5])
+
+# utworzenie nowego obiektu Axes, ze wspólną osią x z sp1 oraz
+# utworzenie serii danych n(t) na tym obiekcie
 sp2.twinx().plot(ZawProp.values[0], ZawProp.values[1], c='green')
+
+# ustawienie układu na obiekcie fig1
 fig1.tight_layout()
+
+# Utworzenie obiektów klasy Figure (1 obiekt) oraz Axes (1 obiekt)
 fig2, sp3 = plt.subplots()
+
+# utworzenie serii danych p(t) na obiekcie sp3
 sp3.plot(ZawProp.values[0], ZawProp.values[5])
-#plt.plot(ZawProp.values[0], ZawProp.values[5])
-#plt.plot(ZawProp.values[0], ZawProp.values[4])
-#plt.plot(ZawProp.values[0], ZawProp.values[3])
-#plt.plot(ZawProp.values[0], ZawProp.values[2])
-#plt.plot(ZawProp.values[0], ZawProp.values[1])
+
+# ustawienie układu na obiekcie fig2
+fig2.tight_layout()
+
+# wyświetlenie wykresów
 plt.show()
 
 
